4363c04a5510bdf150a1cc41787638db02bccda1
fatima <fatima.altamirano@davinci.edu.ar>
2025-10-13 20:26:19 -0300

    metodos http

--- incidencia/views.py
+++ incidencia/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status, viewsets
+from .serializers import IncidenciaCreateSerializer, IncidenciaSerializer
+from .models import Incidencia
 
-# Create your views here.
+class IncidenciaView(viewsets.ModelViewSet):
+    queryset = Incidencia.objects.all()
+    serializer_class = IncidenciaSerializer
+
+    def get_serializer_class(self):
+        if self.action =='create':
+            return IncidenciaCreateSerializer
+        return IncidenciaSerializer
+    
+    
--- usuario/views.py
+++ usuario/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view
 from rest_framework import status, viewsets
 from .serializers import UserSerializer, CreateUserSerializer, ChangePasswordSerializer
 from .models import Usuario
+from turno.models import Turno
+from turno.serializers import TurnoListSerializer
 
 # Create your views here.
 class UserView(viewsets.ModelViewSet):
@@ -37,6 +39,30 @@ class UserView(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
+    @api_view(['PUT']) 
+    def update(self, request, pk=None):
+        usuario = get_object_or_404(Usuario, pk=pk)
+        serializer = self.get_serializer(usuario, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @api_view(['DELETE'])
+    def destroy(self, request, pk=None):
+        usuario = get_object_or_404(Usuario, pk=pk)
+        usuario.activo = False
+        usuario.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+     
+    @api_view(['GET'])
+    def turnos_usuario(self, request, pk=None):
+        usuario = get_object_or_404(Usuario, pk=pk)       
+        turnos = Turno.objects.filter(usuario_idusuario=usuario)
+        serializer = TurnoListSerializer(turnos, many=True)
+        return Response(serializer.data)
     
 
 
