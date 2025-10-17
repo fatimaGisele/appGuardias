29b6011b0a462d6c91e1e4bc263c771b0010b871
fatima <fatima.altamirano@davinci.edu.ar>
2025-10-17 11:29:21 -0300

    trabajando en peticioneshttp

--- incidencia/views.py
+++ incidencia/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -14,4 +15,37 @@ class IncidenciaView(viewsets.ModelViewSet):
             return IncidenciaCreateSerializer
         return IncidenciaSerializer
     
+    #filtra y lista por el estado y la gravedad
+    @api_view(['GET'])
+    def list(self, request):
+        incidencia = self.queryset.all()
+        gravedad = request.query_params.get('gravedad', None)
+        estado = request.query_params.get('estado', None)
+        if estado:
+            incidencia = incidencia.filter(estado=estado)
+        if gravedad:
+            incidencia = incidencia.filter(gravedad=gravedad)
+
+        serializer = self.get_serializer(incidencia, many =True)
+        return Response(serializer.data)
     
+    #VA A marcar una incidencia como resuelta
+    @api_view(['POST'])
+    def resueltas(self, request, pk=None):
+        incidencia = get_object_or_404(Incidencia, pk=pk)
+        incidencia.estado = 'resuelta'
+        incidencia.fecha_resolucion = datetime.now()
+        incidencia.save()
+        
+        serializer = self.get_serializer(incidencia)
+        return Response(serializer.data)
+    
+    # Filtra para obtener incidencias críticas abiertas
+    @api_view(['GET'])
+    def incidencias_criticas_abiertas(self, request):
+        criticas = self.queryset.filter(
+            gravedad='critica',
+            estado='abierta'
+        )
+        serializer = self.get_serializer(criticas, many=True)
+        return Response(serializer.data)
--- relevo/views.py
+++ relevo/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status, viewsets
+from .serializers import RelevoCreateSerializer, RelevoSerializer
+from .models import Relevo
 
 # Create your views here.
+class RelevoView(viewsets.ModelViewSet):
+    queryset = Relevo.objects.all()
+    serializer_class = RelevoSerializer
+
+    def get_serializer_class(self):
+        if self.action =='create':
+            return RelevoCreateSerializer
+        return RelevoCreateSerializer
+    
+    #aceptar un relevo
+    @api_view(['POST'])
+    def aceptar_relevo(self, request, pk=None):
+        relevo = get_object_or_404(relevo, pk=pk)
+        if relevo.estado != 'solicitado':
+            return Response(
+                {'error': 'Solo se pueden aceptar relevos en estado solicitado'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        relevo.estado = 'aceptado'
+        relevo.fecha_respuesta = datetime.now()
+        relevo.save()
+        serializer = self.get_serializer(relevo)
+        return Response(serializer.data)
+    
+     # Obtener unn lista derelevos pendientes
+    @api_view(['GET'])
+    def relevos_pendientes(self, request):
+        pendientes = self.queryset.filter(estado='solicitado')
+        serializer = self.get_serializer(pendientes, many=True)
+        return Response(serializer.data)
+    
