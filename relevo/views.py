6cd4044014ccc96d94cda7acbf3ef91b52938092
fatima <fatima.altamirano@davinci.edu.ar>
2025-10-20 15:04:34 -0300

    arreglos

--- relevo/views.py
+++ relevo/views.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status, viewsets
 from .serializers import RelevoCreateSerializer, RelevoSerializer
 from .models import Relevo
